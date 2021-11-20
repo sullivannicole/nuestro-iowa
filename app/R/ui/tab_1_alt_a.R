98da2ed6a7b8bfadb24767ea8f10e9176e33bbb4
sullivannicole <nicasull@gmail.com>
2021-11-20 08:08:27 -0600

    Add translations for cards 1 and 2

--- app/R/ui/tab_1_alt_a.R
+++ app/R/ui/tab_1_alt_a.R
@@ -8,9 +8,15 @@ card1_english <- argonColumn(h4("Latinx Community Highlights"),
                              br(),
                              HTML("<li>Latinx community turned around over a decade of pop. decline in Marshall County</li>"))
 
-card1_spanish <- "Cosby sweater eu banh mi, qui irure terry richardson ex squid. 
-            Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan 
-            american apparel, butcher voluptate nisi qui."
+card1_spanish <- rgonColumn(h4("Aspectos destacados de la comunidad latina"),
+                            br(),
+                            div(HTML("<li><b>Hay 1,544 negocios latinos que generan <b>$159.6 millones</b> de dólares en ingresos anuales</li>")),
+                            br(),
+                            div(HTML("<li><b>Hay 52 mil residentes latinos en el área metropolitana, una cifra que se duplicará para el año 2050 e impulsará el crecimiento de la población general de la zona metropolitana.</li>")),
+                            br(),
+                            div(HTML("<li><b>Hay 22 mil trabajadores latinos que, colectivamente, generan mil millones de dólares en ingresos anuales, lo que representa una contribución importante a la economía de la región central de Iowa.</li>")),
+                            br(),
+                            HTML("<li>La comunidad latina transformó más de una década de pérdida de población en el condado de Marshall.</li>"))
 
 card2_english <- argonColumn(h4("Disparities (Compared with Whites)"),
                              div(HTML("<li>Average household income is <b>33% lower</b></li>")),
@@ -20,9 +26,13 @@ card2_english <- argonColumn(h4("Disparities (Compared with Whites)"),
                              div(HTML("<li><b>25 percentage point gap</b> in Bachelor's degree or higher completion</li>")),
                              div(HTML("<li><b>33 percentage point gap</b> in high school completion</li>")))
 
-card2_spanish <- "Cosby sweater eu banh mi, qui irure terry richardson ex squid. 
-            Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan 
-            american apparel, butcher voluptate nisi qui."
+card2_spanish <- argonColumn(h4("Disparidades (en comparación con los blancos)"),
+                             div(HTML("<li>Los ingresos familiares promedio son <b>33% más bajos</b></li>")),
+                             div(HTML("<li>La tasa de pobreza es <b>13 puntos porcentuales más alta</b></li>")),
+                             div(HTML("<li>La tasa de propiedad de vivienda es <b>24 puntos porcentuales más baja</b></li>")),
+                             div(HTML("<li>Existe una brecha de <b>21 puntos porcentuales</b> en la cobertura médica para las personas de entre 19 y 64 años.</li>")),
+                             div(HTML("<li>Existe una brecha de <b>25 puntos porcentuales </b>en la culminación de estudios universitarios a nivel de licenciatura o postgrado</li>")),
+                             div(HTML("<li><b>Existe una brecha de <b>33 puntos porcentuales</b> en la culminación de estudios de preparatoria</li>")))
 
 card3_english <- argonColumn(h4("Root causes of disparities"),
                              div(HTML("<li><b>Eliminating education disparities</b> will largely close the income and poverty gap</li>")),
