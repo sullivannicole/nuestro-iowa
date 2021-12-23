610c0104f2e50fed9885447c89406d853f3fe3e6
sullivannicole <nicasull@gmail.com>
2021-12-23 15:42:01 -0600

    Switch white logo to colored logo

--- app/R/elements/03_footer.R
+++ app/R/elements/03_footer.R
@@ -1,6 +1,6 @@
 argonFooter <- argonDashFooter(
   
-  copyrights = argonImage(src = "logo_white.png", url = "https://www.iowalatinxproject.org/", width = 150),
+  copyrights = argonImage(src = "logo.svg", url = "https://www.iowalatinxproject.org/", width = 150),
   HTML("<br><b style = 'color: white !important; font-size: 12px !important;'>For questions about this dashboard, contact Joel Huting by emailing Huting@vandegrifthuting.com. <br> 
        Landing page images created by Ted Grajeda for the Noun Project</b>")
   
