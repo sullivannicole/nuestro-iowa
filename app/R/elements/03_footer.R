4f3184abe0696eb5b284fe728ed35092b7e04e62
sullivannicole <nicasull@gmail.com>
2021-12-23 14:56:31 -0600

    Incorporate logo into footer. Link the project website

--- app/R/elements/03_footer.R
+++ app/R/elements/03_footer.R
@@ -1,10 +1,7 @@
 argonFooter <- argonDashFooter(
-  copyrights = "Iowa Latinx Project, 2021",
-  HTML("For questions about this dashboard, contact Joel Huting by emailing <b style = 'color: white !important; font-size: 12px !important;'>Huting@vandegrifthuting.com. <br> 
-       Landing page images created by Ted Grajeda for the Noun Project</b>"),
-  src = "www.iowalatinxproject.org"
-  # argonFooterMenu(
-  #   argonFooterItem("RinteRface", src = "https://github.com/RinteRface"),
-  #   argonFooterItem("argon", src = "https://demos.creative-tim.com/argon-design-system/index.html")
-  # )
+  
+  copyrights = argonImage(src = "logo_white.png", url = "https://www.iowalatinxproject.org/", width = 150),
+  HTML("<br><b style = 'color: white !important; font-size: 12px !important;'>For questions about this dashboard, contact Joel Huting by emailing Huting@vandegrifthuting.com. <br> 
+       Landing page images created by Ted Grajeda for the Noun Project</b>")
+  
 )
