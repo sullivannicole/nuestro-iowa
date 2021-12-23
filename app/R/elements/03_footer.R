083dbb5bca64b1c7a36ea9234011b816c2f3ef88
sullivannicole <nicasull@gmail.com>
2021-12-23 14:24:20 -0600

    Add Joel's contact info and link to main website

--- app/R/elements/03_footer.R
+++ app/R/elements/03_footer.R
@@ -1,8 +1,8 @@
 argonFooter <- argonDashFooter(
   copyrights = "Iowa Latinx Project, 2021",
-  HTML("<b style = 'color: white !important; font-size: 12px !important;'>joel@vandegrifthuting.com <br> 
-       Landing page images created by Ted Grajeda for the Noun Project</b>")
-  # src = "https://www.alexitoiowa.org",
+  HTML("For questions about this dashboard, contact Joel Huting by emailing <b style = 'color: white !important; font-size: 12px !important;'>Huting@vandegrifthuting.com. <br> 
+       Landing page images created by Ted Grajeda for the Noun Project</b>"),
+  src = "www.iowalatinxproject.org"
   # argonFooterMenu(
   #   argonFooterItem("RinteRface", src = "https://github.com/RinteRface"),
   #   argonFooterItem("argon", src = "https://demos.creative-tim.com/argon-design-system/index.html")
