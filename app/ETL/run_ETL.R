d9f35bd5623c0250887fe6fec73774dcaad64142
Nicole Sullivan <nicolesullivan@Nicoles-MacBook-Pro.local>
2021-12-31 07:30:26 -0600

    Change wd

--- app/ETL/run_ETL.R
+++ app/ETL/run_ETL.R
@@ -9,7 +9,8 @@ library(tidycensus)
 
 acs_yr <- 2019
 # census_api_key(key_get("census_api"), install = TRUE) #only needs to be done once per machine
-setwd("/Users/nicolesullivan/Documents/Professional/VandegriftHuting_consulting/nuestro-iowa/app/ETL/")
+# setwd("/Users/nicolesullivan/Documents/Professional/VandegriftHuting_consulting/nuestro-iowa/app/ETL/")
+setwd("/Volumes/NAS T7/Professional/VandegriftHuting_consulting/nuestro-iowa/app/ETL")
 
 #-----------------------------------
 
